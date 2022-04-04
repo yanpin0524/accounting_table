8f5b5505b24627a5ebf487a73ddb854a9eb8194e
Leo <a131234568@gmail.com>
2022-04-04 13:59:10 +0800

    專案初始化

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.listen(PORT, () => {
+  console.log(`【 網站已開啟 : localhost:${PORT} 】`)
+})
