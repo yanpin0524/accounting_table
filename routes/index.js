d2f8b8c21420d3d855d68102132924d3986f571a
Leo <a131234568@gmail.com>
2022-04-05 01:47:58 +0800

    完成新增功能頁面

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const accounting = require('./modules/accounting')
 
+router.use('/accounting', accounting)
 router.use('/', home)
 
 module.exports = router
---
+++ routes/modules/accounting.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const router = express.Router()
+
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/new', (req, res) => {
+  Category.find({})
+    .lean()
+    .then(catagories => {
+      return res.render('new', { catagories })
+    })
+})
+
+router.post('', (req, res) => {
+  const userId = req.user._id
+  const name = req.body.name
+
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+
+  return Todo.findOne({ _id, userId })
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const { name, isDone } = req.body
+
+  return Todo.findOne({ _id, userId })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${_id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+
+  return Todo.findOne({ _id, userId })
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
