ceeeb85dda22de72b0eb16ea2b97127a1eec7e47
Leo <a131234568@gmail.com>
2022-04-05 23:59:23 +0800

    完成搜尋功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,20 +7,60 @@ const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
-    .populate('categoryId')
-    .lean()
-    .sort({ date: 'desc' }) // asc
-    .then(accountingTable => {
-      let totalAmount = 0
-      accountingTable.forEach(item => {
-        totalAmount += item.amount
-        item.date = moment(item.date).format('YYYY-MM-DD')
-      })
 
-      Category.find({})
+  Category.find({})
+    .lean()
+    .then(catagories => {
+      Record.find({ userId })
+        .populate('categoryId')
         .lean()
-        .then(catagories => {
+        .sort({ date: 'desc' }) // asc
+        .then(accountingTable => {
+          let totalAmount = 0
+          accountingTable.forEach(item => {
+            totalAmount += item.amount
+            item.date = moment(item.date).format('YYYY-MM-DD')
+          })
+
+          return res.render('home', {
+            accountingTable,
+            catagories,
+            totalAmount
+          })
+        })
+    })
+    .catch(error => console.error(error))
+})
+
+router.get('/search', (req, res) => {
+  const userId = req.user._id
+  const categoryId = req.query.categoryId
+
+  if (!categoryId) {
+    return res.redirect('/')
+  }
+
+  Category.find({})
+    .lean()
+    .then(catagories => {
+      catagories.forEach(item => {
+        if (String(item._id) === categoryId) {
+          item.preset = true
+        } else {
+          item.preset = false
+        }
+      })
+      Record.find({ userId, categoryId })
+        .populate('categoryId')
+        .lean()
+        .sort({ date: 'desc' }) // asc
+        .then(accountingTable => {
+          let totalAmount = 0
+          accountingTable.forEach(item => {
+            totalAmount += item.amount
+            item.date = moment(item.date).format('YYYY-MM-DD')
+          })
+
           return res.render('home', {
             accountingTable,
             catagories,
