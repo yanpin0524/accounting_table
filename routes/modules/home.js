61f8c33fc8232bf6071f2aa23aa1252e136f3f3e
Leo <a131234568@gmail.com>
2022-04-05 21:17:34 +0800

    調整 首頁日期格式

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
 
 const Record = require('../../models/record')
 const Category = require('../../models/category')
@@ -7,20 +8,22 @@ const Category = require('../../models/category')
 router.get('/', (req, res) => {
   const userId = req.user._id
   Record.find({ userId })
+    .populate('categoryId')
     .lean()
     .sort({ date: 'desc' }) // asc
     .then(accountingTable => {
       let totalAmount = 0
       accountingTable.forEach(item => {
         totalAmount += item.amount
+        item.date = moment(item.date).format('YYYY-MM-DD')
       })
 
       Category.find({})
         .lean()
         .then(catagories => {
           return res.render('home', {
-            accountingTable, 
-            catagories, 
+            accountingTable,
+            catagories,
             totalAmount
           })
         })
